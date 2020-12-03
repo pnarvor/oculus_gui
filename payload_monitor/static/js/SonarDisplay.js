5eed7ccb9758644ef58384115cbe1581c530c427
Pierre Narvor <pierre.narvor@ensta-bretagne.fr>
2020-12-03 11:16:39 +0100

    [server] Moved SonarDisplay to dedicated script, added sonar_monitor page

---
+++ payload_monitor/static/js/SonarDisplay.js
@@ -0,0 +1,51 @@
+
+class SonarDisplay extends Display
+{
+    constructor(canvas)
+    {
+        super(canvas);
+
+        this.pingRenderer = new SonarRenderer(this.gl);
+        this.add_renderer(this.pingRenderer);
+
+        this.pingListener = new DataListener();
+        //this.pingListener = new DataListener('/ws/subscribe/test/');
+        this.pingListener.callbacks.push(this.ping_callback.bind(this));
+
+        this.busy = false;
+    }
+
+    async ping_callback(content)
+    {
+        // does this "mutex" really is secure ? (this function is
+        // asynchronously called by the callback of a websocket.
+        if(this.busy) {
+            console.log("Busy : ignoring data");
+            return;
+        }
+        this.busy = true;
+        
+        try {
+            let metadata = JSON.parse(content.content.metadata);
+            let data = new Uint8Array(await content.fetch_cached_data());
+
+            this.pingRenderer.set_ping_data(metadata, data.subarray(metadata.imageOffset));
+        }
+        finally {
+            this.busy = false;
+        }
+        this.draw();
+    }
+};
+
+
+$(document).ready(function() {
+    let display = new SonarDisplay($("#main_display")[0]);
+    //window.onresize = function() {
+    //    $("#main_display")[0].naglContext
+    //        .resize(window.innerWidth, window.innerHeight);
+    //};
+    //window.onresize();
+
+    $('.sidenav').sidenav({edge:'right'});
+});
---
+++ payload_monitor/static/js/sonar_monitor.js
@@ -0,0 +1,14 @@
+
+
+
+
+$(document).ready(function() {
+    let display = new SonarDisplay($("#main_display")[0]);
+    //window.onresize = function() {
+    //    $("#main_display")[0].naglContext
+    //        .resize(window.innerWidth, window.innerHeight);
+    //};
+    //window.onresize();
+
+    $('.sidenav').sidenav({edge:'right'});
+});
